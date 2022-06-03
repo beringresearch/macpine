d2be77d63eb02f7987ce5f562af1cac444eabb02
idroz <idrozdov@beringresearch.com>
2022-06-03 23:35:08 +0100

    nicely handle aliases and paths when importing a renamed vm

--- cmd/import.go
+++ cmd/import.go
@@ -1,13 +1,16 @@
 package cmd
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 
+	qemu "github.com/beringresearch/macpine/qemu"
 	"github.com/beringresearch/macpine/utils"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
 // importCmd iports an Alpine VM from file
@@ -21,7 +24,6 @@ func importMachine(cmd *cobra.Command, args []string) {
 
 	if len(args) == 0 {
 		log.Fatal("missing VN name")
-		return
 	}
 
 	userHomeDir, err := os.UserHomeDir()
@@ -36,6 +38,37 @@ func importMachine(cmd *cobra.Command, args []string) {
 
 	targetDir := filepath.Join(userHomeDir, ".macpine", strings.Split(args[0], ".tar.gz")[0])
 	err = utils.Uncompress(filepath.Join(userHomeDir, ".macpine", args[0]), targetDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	machineConfig := qemu.MachineConfig{}
+	config, err := ioutil.ReadFile(filepath.Join(targetDir, "config.yaml"))
+	if err != nil {
+		os.RemoveAll(targetDir)
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(config, &machineConfig)
+	if err != nil {
+		os.RemoveAll(targetDir)
+		log.Fatal(err)
+	}
+
+	machineConfig.Alias = strings.Split(args[0], ".tar.gz")[0]
+	machineConfig.Location = targetDir
+
+	updatedConfig, err := yaml.Marshal(&machineConfig)
+	if err != nil {
+		os.RemoveAll(targetDir)
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(filepath.Join(targetDir, "config.yaml"), updatedConfig, 0644)
+	if err != nil {
+		os.RemoveAll(targetDir)
+		log.Fatal(err)
+	}
 
 	if err != nil {
 		err = os.Remove(filepath.Join(userHomeDir, ".macpine", args[0]))
