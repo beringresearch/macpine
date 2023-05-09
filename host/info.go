eb4f403f8a1903c31e4b08761324c8da09e3f34e
Max Zinkus <5474655+maxzinkus@users.noreply.github.com>
2023-05-08 21:02:14 -0500

    Use Get/Save utils in host/

--- host/info.go
+++ host/info.go
@@ -2,28 +2,12 @@ package host
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 
 	"github.com/beringresearch/macpine/qemu"
-	"gopkg.in/yaml.v3"
 )
 
 func Info(vmName string) (string, error) {
-	userHomeDir, err := os.UserHomeDir()
-	if err != nil {
-		return "", err
-	}
-
-	config, err := ioutil.ReadFile(filepath.Join(userHomeDir, ".macpine", vmName, "config.yaml"))
-	if err != nil {
-		return "", err
-	}
-
-	machineConfig := qemu.MachineConfig{}
-
-	err = yaml.Unmarshal(config, &machineConfig)
+	machineConfig, err := qemu.GetMachineConfig(vmName)
 	if err != nil {
 		return "", err
 	}
