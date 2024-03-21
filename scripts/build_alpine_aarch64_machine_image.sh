54b6aff99397d1030c96a5f5f2f25917fbe34099
idroz <idrozdov@beringresearch.com>
2024-03-21 18:06:42 +0000

    add download script for latest alpine image

---
+++ scripts/build_alpine_aarch64_machine_image.sh
@@ -0,0 +1,13 @@
+VERSION=3.19
+MINORVERSION=1
+ARCH="aarch64"
+ISO=https://dl-cdn.alpinelinux.org/alpine/v$VERSION/releases/$ARCH/alpine-standard-$VERSION.$MINORVERSION-$ARCH.iso
+
+
+wget $ISO
+
+
+# inside the vm run alpine-setup
+# Allow root ssh login: yes
+# Which disk you would like to use: sda
+# How would you like to use it: sys
